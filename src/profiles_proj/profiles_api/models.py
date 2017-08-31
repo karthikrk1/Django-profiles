9e22e7c1f6f6e97fda966233c03ea85760c24362
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-08-30 20:29:08 -0500

    Created a custom User Model

--- src/profiles_proj/profiles_api/models.py
+++ src/profiles_proj/profiles_api/models.py
@@ -2,5 +2,38 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represents a "user profile" inside our system.
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Used to get a user's full name"""
+
+        return self.name
+
+    def get_abbr_name(self):
+        """Used to get the short name"""
+
+        return self.name
+
+    def __str__(self):
+        """Used to convert an object to text"""
+
+        return self.email
+        
