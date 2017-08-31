bc448ca582240410de7bf602c95dfcf76bf03601
Karthik Ramakrishnan <karthik.ramakrishnan20@gmail.com>
2017-08-30 20:40:49 -0500

    Added a new User Profile Manager

--- src/profiles_proj/profiles_api/models.py
+++ src/profiles_proj/profiles_api/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 
@@ -36,4 +37,32 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Used to convert an object to text"""
 
         return self.email
-        
+
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our custom User Manager"""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new User Profile Object"""
+
+        if not email:
+            raise ValueError('Email is a required field')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Creates a superuser with the given details."""
+
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(self._db)
+
+        return user
